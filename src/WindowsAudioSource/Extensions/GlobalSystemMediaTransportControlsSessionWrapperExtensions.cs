3e237a05c4495e7d7ac19930d19952936947239d
Nate Peterson <ntpeters@mtu.edu>
2021-04-26 12:35:33 -0700

    Add helper extensions for checking session capabilities

---
+++ src/WindowsAudioSource/Extensions/GlobalSystemMediaTransportControlsSessionWrapperExtensions.cs
@@ -0,0 +1,54 @@
+using WindowsAudioSource.Wrappers;
+
+namespace WindowsAudioSource.Extensions
+{
+    public static class GlobalSystemMediaTransportControlsSessionWrapperExtensions
+    {
+        public static bool TryGetIsPlayPauseCapable(this IGlobalSystemMediaTransportControlsSessionWrapper session, out bool isCapable)
+        {
+            if (session.TryGetControls(out var controls))
+            {
+                isCapable = controls.IsPlayEnabled || controls.IsPauseEnabled || controls.IsPlayPauseToggleEnabled;
+                return true;
+            }
+
+            isCapable = false;
+            return false;
+        }
+
+        public static bool TryGetIsNextPreviousCapable(this IGlobalSystemMediaTransportControlsSessionWrapper session, out bool isCapable)
+        {
+            if (session.TryGetControls(out var controls))
+            {
+                isCapable = controls.IsNextEnabled || controls.IsPreviousEnabled;
+                return true;
+            }
+
+            isCapable = false;
+            return false;
+        }
+
+        public static bool TryGetIsPlaybackPositionCapable(this IGlobalSystemMediaTransportControlsSessionWrapper session, out bool isCapable)
+        {
+            if (session.TryGetControls(out var controls))
+            {
+                isCapable = controls.IsPlaybackPositionEnabled;
+                return true;
+            }
+
+            isCapable = false;
+            return false;
+        }
+
+        private static bool TryGetControls(this IGlobalSystemMediaTransportControlsSessionWrapper session, out IGlobalSystemMediaTransportControlsSessionPlaybackControlsWrapper controls)
+        {
+            controls = session?.GetPlaybackInfo()?.Controls;
+            if (controls != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
