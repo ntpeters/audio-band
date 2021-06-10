495092f6951e22590f64061238ed0e4bd93e5841
Nate Peterson <ntpeters@mtu.edu>
2021-06-10 11:32:43 -0700

    Add tests for TypedEventHandlerExtensions

--- src/WindowsAudioSource.Test/TypedEventHandlerExtensionsTest.cs
+++ src/WindowsAudioSource.Test/TypedEventHandlerExtensionsTest.cs
@@ -1,17 +1,37 @@
-using Moq;
-using System;
+using Windows.Foundation;
 using WindowsAudioSource.Extensions;
 using Xunit;
 
 namespace WindowsAudioSource.Test
 {
-    // TODO: TypedEventHandlerExtensions tests
     public class TypedEventHandlerExtensionsTests
     {
+        private event TypedEventHandler<TypedEventHandlerExtensionsTests, string> TestEvent;
+
         [Fact]
-        public void HasSubscribers()
+        public void HasSubscribers_WithNoSubscribersAdded_ReturnsFalse()
         {
-            Assert.False(true, "Implement Me!");
+            Assert.False(TestEvent.HasSubscribers());
+        }
+
+        [Fact]
+        public void HasSubscribers_WithSubscribersAdded_ReturnsTrue()
+        {
+            TestEvent += OnTestEvent;
+            Assert.True(TestEvent.HasSubscribers());
+        }
+
+        [Fact]
+        public void HasSubscribers_WithAllSubscribersRemoved_ReturnsFalse()
+        {
+            TestEvent += OnTestEvent;
+            TestEvent -= OnTestEvent;
+            Assert.False(TestEvent.HasSubscribers());
+        }
+
+        // Dummy event handler for testing
+        private void OnTestEvent(TypedEventHandlerExtensionsTests sender, string args)
+        {
         }
     }
 }
