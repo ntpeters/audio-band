d9be5fc7880740ef9f0cfe5c9e27633cda4d51b2
Nate Peterson <ntpeters@mtu.edu>
2021-06-10 10:40:26 -0700

    Add tests and comments for EnumerableExtensions

--- src/WindowsAudioSource.Test/EnumerableExtensionsTests.cs
+++ src/WindowsAudioSource.Test/EnumerableExtensionsTests.cs
@@ -1,17 +1,63 @@
-using Moq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using WindowsAudioSource.Extensions;
 using Xunit;
 
 namespace WindowsAudioSource.Test
 {
-    // TODO: EnumerableExtensions tests
     public class EnumerableExtensionsTests
     {
         [Fact]
-        public void FirstInGroupOrDefault()
+        public void FirstInGroupOrDefault_SingleValueInMatchedGroup_ReturnsValue()
         {
-            Assert.False(true, "Implement Me!");
+            var list = new List<string>
+            {
+                "1-one",
+                "2-two",
+                "3-three"
+            };
+            var groups = list.GroupBy(value => Convert.ToInt32(value.Split('-')[0]));
+
+            Assert.Equal("2-two", groups.FirstInGroupOrDefault(2));
+        }
+
+        [Fact]
+        public void FirstInGroupOrDefault_MultipleValuesMatchedInGroup_ReturnsFirstValue()
+        {
+            var list = new List<string>
+            {
+                "1-one",
+                "2-two",
+                "2-three",
+                "3-four"
+            };
+            var groups = list.GroupBy(value => Convert.ToInt32(value.Split('-')[0]));
+
+            Assert.Equal("2-two", groups.FirstInGroupOrDefault(2));
+        }
+
+        [Fact]
+        public void FirstInGroupOrDefault_NoMatchingGroup_ReturnsDefault()
+        {
+            var list = new List<string>
+            {
+                "1-one",
+                "2-two",
+                "3-three"
+            };
+            var groups = list.GroupBy(value => Convert.ToInt32(value.Split('-')[0]));
+
+            Assert.Equal(default(string), groups.FirstInGroupOrDefault(5));
+        }
+
+        [Fact]
+        public void FirstInGroupOrDefault_EmptySequence_ReturnsDefault()
+        {
+            var list = new List<int>();
+            var groups = list.GroupBy(value => value);
+
+            Assert.Equal(default(int), groups.FirstInGroupOrDefault(1));
         }
     }
 }
--- src/WindowsAudioSource/Extensions/EnumerableExtensions.cs
+++ src/WindowsAudioSource/Extensions/EnumerableExtensions.cs
@@ -5,8 +5,17 @@ namespace WindowsAudioSource.Extensions
 {
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Returns the first element in the first group of a sequence matching the given <paramref name="key"/>, or a default value if the sequence is empty, no matching group is found, or the matching group is empty.
+        /// </summary>
+        /// <typeparam name="TGroupKey">Type of the key of the <see cref="IGrouping{TKey, TElement}"/> items in the sequence.</typeparam>
+        /// <typeparam name="TGroupElement">Type of the values of the <see cref="IGrouping{TKey, TElement}"/> items in the sequence.</typeparam>
+        /// <param name="groupedElements">An <see cref="IEnumerable{T}"/> of <see cref="IGrouping{TKey, TElement}"/></param>
+        /// <param name="key">Key of the <see cref="IGrouping{TKey, TElement}"/> to get the first element of in the sequence.</param>
+        /// <returns>
+        /// <see langword="default"/>(<typeparamref name="TGroupElement"/>) if <paramref name="groupedElements"/> is empty, does not contain a group matching the <paramref name="key"/>, or the matching group is empty; otherwise, the first element in the first group matching the <paramref name="key"/>.
+        /// </returns>
         public static TGroupElement FirstInGroupOrDefault<TGroupKey, TGroupElement>(this IEnumerable<IGrouping<TGroupKey, TGroupElement>> groupedElements, TGroupKey key)
-            where TGroupKey : struct
         {
             var keyGroup = groupedElements.FirstOrDefault(group => group.Key.Equals(key));
             if (keyGroup == null)
